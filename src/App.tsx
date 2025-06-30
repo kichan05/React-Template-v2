4f88e37b2c51d5eddfefb707d0a20c5883d57875
바키찬 <ckstmznf@naver.com>
2025-06-30 18:37:09 +0900

    Feat: Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import {MainPage} from "./MainPage";
 import {Header} from "./Header";
-
+import {Footer} from "./Footer";
 
 function App() {
     return (
         <>
             <Header/>
             <MainPage/>
+            <Footer/>
         </>
     )
 }
